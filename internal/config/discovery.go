5b47db1bd3ed81c5864520203ca4b88a54e6962c
Carlos Fdez. Llamas <hello@sirikon.me>
2024-10-31 12:16:54 +0100

    wip

--- internal/config/discovery.go
+++ internal/config/discovery.go
@@ -7,14 +7,18 @@ import (
 )
 
 var ErrDiscoveryFailed = errors.New("discovery failed")
-var discoveryPaths = []string{"Ebro.yaml", "Ebro.yml"}
+var discoveryPaths = []string{"Ebro.yaml"}
 
 func DiscoverModule() (*Module, error) {
 	for _, path := range discoveryPaths {
-		module, err := parseModuleFromFile(path)
-		if errors.Is(err, os.ErrNotExist) {
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 			continue
 		} else if err != nil {
+			return nil, fmt.Errorf("discovery on file %v: %w", path, err)
+		}
+
+		module, err := parseModuleFromFile(path)
+		if err != nil {
 			return nil, fmt.Errorf("discovery on file %v: %w", path, err)
 		}
 		return module, nil
--- internal/config/parser.go
+++ internal/config/parser.go
@@ -3,23 +3,41 @@ package config
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"gopkg.in/yaml.v3"
 )
 
-func parseModuleFromFile(path string) (*Module, error) {
+func parseModuleFromFile(filePath string) (*Module, error) {
 	module := Module{}
-	body, err := os.ReadFile(path)
+
+	body, err := os.ReadFile(filePath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("parsing %v: %w", filePath, err)
 	}
 	err = yaml.Unmarshal(body, &module)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("parsing %v: %w", filePath, err)
 	}
 	err = module.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("parsing %v: %w", path, err)
+		return nil, fmt.Errorf("parsing %v: %w", filePath, err)
 	}
+
+	for name, config := range module.Imports {
+		_, ok := module.Modules[name]
+		if ok {
+			return nil, fmt.Errorf("parsing %v: trying to import module %v, but it already exists", filePath, name)
+		}
+		submodule, err := parseModuleFromFile(path.Join(config.From, "Ebro.yaml"))
+		if err != nil {
+			return nil, fmt.Errorf("parsing %v: %w", filePath, err)
+		}
+		if module.Modules == nil {
+			module.Modules = make(map[string]Module)
+		}
+		module.Modules[name] = *submodule
+	}
+
 	return &module, nil
 }
