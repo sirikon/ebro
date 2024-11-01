a705604eef0e047706bdcbc8ceb34bb8a84c65c2
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-01 19:24:55 +0100

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/sirikon/ebro
 
 go 1.23.2
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	gopkg.in/yaml.v3 v3.0.1
+	mvdan.cc/sh/v3 v3.10.0
+)
+
+require (
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
+)
--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -1,14 +1,31 @@
 package runner
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"strings"
 
 	"github.com/sirikon/ebro/internal/indexer"
 	"github.com/sirikon/ebro/internal/planner"
+	"mvdan.cc/sh/v3/interp"
+	"mvdan.cc/sh/v3/syntax"
 )
 
 func Run(index indexer.Index, plan planner.Plan) {
 	for _, task_name := range plan {
-		fmt.Println(task_name)
+		task := index[task_name]
+		if task.Script != "" {
+			runScript(task.Script)
+		}
 	}
+
+}
+
+func runScript(script string) {
+	file, _ := syntax.NewParser().Parse(strings.NewReader(script), "")
+	runner, _ := interp.New(
+		interp.Env(nil),
+		interp.StdIO(nil, os.Stdout, os.Stderr),
+	)
+	runner.Run(context.TODO(), file)
 }
