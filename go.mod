281fc4a54d9ebdc820b8eea81d199c1790c155be
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-02 16:53:35 +0100

    some logs during execution

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/sirikon/ebro
 go 1.23.2
 
 require (
+	github.com/fatih/color v1.18.0
 	gopkg.in/yaml.v3 v3.0.1
 	mvdan.cc/sh/v3 v3.10.0
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,33 +2,72 @@ package runner
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"strings"
 
-	"github.com/sirikon/ebro/internal/cataloger"
-	"github.com/sirikon/ebro/internal/planner"
+	"github.com/fatih/color"
 	"mvdan.cc/sh/v3/expand"
 	"mvdan.cc/sh/v3/interp"
 	"mvdan.cc/sh/v3/syntax"
+
+	"github.com/sirikon/ebro/internal/cataloger"
+	"github.com/sirikon/ebro/internal/planner"
 )
 
-func Run(catalog cataloger.Catalog, plan planner.Plan) {
+func Run(catalog cataloger.Catalog, plan planner.Plan) error {
 	for _, task_name := range plan {
 		task := catalog[task_name]
+		if task.SkipIf != "" {
+			status, err := runScript(task.Script, *task.WorkingDirectory, task.Environment)
+			if err != nil {
+				return fmt.Errorf("running task %v skip_if: %w", task_name, err)
+			}
+			if status == 0 {
+				color.Green("### skipping " + task_name)
+				continue
+			}
+		}
+
 		if task.Script != "" {
-			runScript(task.Script, *task.WorkingDirectory, task.Environment)
+			color.Yellow("### running " + task_name)
+			status, err := runScript(task.Script, *task.WorkingDirectory, task.Environment)
+			if err != nil {
+				return fmt.Errorf("running task %v script: %w", task_name, err)
+			}
+			if status != 0 {
+				return fmt.Errorf("task %v returned status code %v", task_name, status)
+			}
 		}
 	}
+	return nil
 }
 
-func runScript(script string, working_directory string, environment map[string]string) {
-	file, _ := syntax.NewParser().Parse(strings.NewReader(script), "")
-	runner, _ := interp.New(
+func runScript(script string, working_directory string, environment map[string]string) (uint8, error) {
+	file, err := syntax.NewParser().Parse(strings.NewReader(script), "")
+	if err != nil {
+		return 1, fmt.Errorf("parsing script: %w", err)
+	}
+
+	runner, err := interp.New(
 		interp.Env(expand.ListEnviron(append(os.Environ(), environmentToString(environment)...)...)),
 		interp.Dir(working_directory),
 		interp.StdIO(nil, os.Stdout, os.Stderr),
 	)
-	runner.Run(context.TODO(), file)
+	if err != nil {
+		return 1, fmt.Errorf("runner creation failed: %w", err)
+	}
+
+	err = runner.Run(context.TODO(), file)
+	if err == nil {
+		return 0, nil
+	}
+
+	if status, ok := interp.IsExitStatus(err); ok {
+		return status, nil
+	} else {
+		return 1, fmt.Errorf("runner returned error: %w", err)
+	}
 }
 
 func environmentToString(environment map[string]string) []string {
