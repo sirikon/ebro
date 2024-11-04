b24b9283a6a13d9fa42fe07c9055d7b5b14ee90b
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-04 19:20:00 +0100

    shell expansion working directory

--- internal/cataloger/cataloger.go
+++ internal/cataloger/cataloger.go
@@ -39,10 +39,17 @@ func NormalizeTaskReferences(catalog Catalog, task_names []string) {
 func catalogModule(module *config.Module, name_trail []string, working_directory *string, environment map[string]string) (Catalog, error) {
 	result := make(Catalog)
 	prefix := ":" + strings.Join(append(name_trail, ""), ":")
+	module.Environment = utils.MergeEnv(environment, module.Environment)
 	if module.WorkingDirectory == nil {
 		module.WorkingDirectory = working_directory
+	} else {
+		expanded_working_directory, err := expand(*module.WorkingDirectory, module.Environment)
+		if err != nil {
+			return nil, fmt.Errorf("expanding source %v: %w", module.WorkingDirectory, err)
+		}
+		module.WorkingDirectory = &expanded_working_directory
 	}
-	module.Environment = utils.MergeEnv(environment, module.Environment)
+
 	if _, ok := module.Environment["EBRO_ROOT"]; !ok {
 		module.Environment["EBRO_ROOT"] = *module.WorkingDirectory
 	}
@@ -58,18 +65,19 @@ func catalogModule(module *config.Module, name_trail []string, working_directory
 				task.RequiredBy[i] = prefix + task.RequiredBy[i]
 			}
 		}
+		task.Environment = utils.MergeEnv(module.Environment, task.Environment)
 		if task.WorkingDirectory == nil {
 			task.WorkingDirectory = module.WorkingDirectory
+		} else {
+			expanded_working_directory, err := expand(*task.WorkingDirectory, task.Environment)
+			if err != nil {
+				return nil, fmt.Errorf("expanding source %v for task %v: %w", task.WorkingDirectory, task_name, err)
+			}
+			task.WorkingDirectory = &expanded_working_directory
 		}
-		task.Environment = utils.MergeEnv(module.Environment, task.Environment)
 		if task.Sources != nil {
 			for i, source := range task.Sources {
-				expanded_source, err := shell.Expand(source, func(s string) string {
-					if val, ok := task.Environment[s]; ok {
-						return val
-					}
-					return os.Getenv(s)
-				})
+				expanded_source, err := expand(source, task.Environment)
 				if err != nil {
 					return nil, fmt.Errorf("expanding source %v for task %v: %w", source, task_name, err)
 				}
@@ -94,4 +102,13 @@ func catalogModule(module *config.Module, name_trail []string, working_directory
 	}
 
 	return result, nil
+}
+
+func expand(s string, env map[string]string) (string, error) {
+	return shell.Expand(s, func(s string) string {
+		if val, ok := env[s]; ok {
+			return val
+		}
+		return os.Getenv(s)
+	})
 }
