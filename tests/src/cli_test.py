980608e2a81cef6548469b99c65e146f05ee8100
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-06 22:30:32 +0100

    add docs

--- tests/src/cli_test.py
+++ tests/src/cli_test.py
@@ -12,29 +12,34 @@ class TestCli(EbroTestCase):
             Usage: ebro [-command?] [--flags?...] [targets?...]
 
             Available commands:
-              -help
-              -version
-              -config
-              -catalog
-              -plan
+              -help     Displays this help message
+              -version  Display ebro's version
+              -config   Display all imported configuration files merged into one
+              -catalog  Display complete catalog of tasks with their definitive configuration
+              -plan     Display the execution plan
 
             Available flags:
-              --file
-              --force
+              --file   Specify the file that should be loaded as root module. default: Ebro.yaml
+              --force  Ignore when.* conditionals and dont skip any task
             """,
         )
 
     def test_unknown_commands_are_reported(self):
         exit_code, stdout = self.ebro("-invent")
         self.assertEqual(exit_code, 1)
-        self.assertStdout(stdout, "ERROR: unknown command: invent")
+        self.assertStdout(stdout, "███ ERROR: unknown command: invent")
 
     def test_unknown_flags_are_reported(self):
         exit_code, stdout = self.ebro("-plan", "--invent")
         self.assertEqual(exit_code, 1)
-        self.assertStdout(stdout, "ERROR: unknown flag: invent")
+        self.assertStdout(stdout, "███ ERROR: unknown flag: invent")
 
     def test_version_is_displayed(self):
         exit_code, stdout = self.ebro("-version")
         self.assertEqual(exit_code, 0)
         self.assertStdout(stdout, "dev")
+
+    def test_file_flag_handles_missing_arg_correctly(self):
+        exit_code, stdout = self.ebro("--file")
+        self.assertEqual(exit_code, 1)
+        self.assertStdout(stdout, "███ ERROR: expected value after --file flag")
