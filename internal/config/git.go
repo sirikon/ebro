349920395304b0e20d78e44d18bf59a60b827822
Carlos Fdez. Llamas <hello@sirikon.me>
2024-11-10 14:02:00 +0100

    Clone modules from git

---
+++ internal/config/git.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/sirikon/ebro/internal/logger"
+)
+
+type gitReference struct {
+	cloneUrl  string
+	clonePath string
+	branch    string
+	subPath   string
+}
+
+func parseGitReference(_url string) (*gitReference, error) {
+	if !strings.HasPrefix(_url, "git+") {
+		return nil, nil
+	}
+	_url = strings.TrimPrefix(_url, "git+")
+	parsedUrl, err := url.Parse(_url)
+	if err != nil {
+		return nil, fmt.Errorf("parsing url %v: %w", _url, err)
+	}
+	branch := "master"
+	subPath := "."
+	if parsedUrl.Fragment != "" {
+		values, err := url.ParseQuery(strings.TrimPrefix(parsedUrl.Fragment, "?"))
+		if err != nil {
+			return nil, fmt.Errorf("parsing fragment of url %v: %w", _url, err)
+		}
+		if val, ok := values["path"]; ok {
+			subPath = val[0]
+		}
+		if val, ok := values["branch"]; ok {
+			branch = val[0]
+		}
+	}
+
+	return &gitReference{
+		cloneUrl:  parsedUrl.Scheme + "://" + parsedUrl.Host + parsedUrl.Path,
+		clonePath: path.Join(".ebro", "git", parsedUrl.Scheme, parsedUrl.Host+parsedUrl.Path, branch),
+		branch:    branch,
+		subPath:   subPath,
+	}, nil
+}
+
+func gitCloneModule(ref *gitReference) error {
+	_, err := os.Stat(ref.clonePath)
+	if err == nil {
+		return nil
+	}
+	if !errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+	logger.Notice("cloning " + ref.cloneUrl)
+	_, err = git.PlainClone(ref.clonePath, false, &git.CloneOptions{
+		URL:           ref.cloneUrl,
+		ReferenceName: plumbing.ReferenceName(ref.branch),
+		SingleBranch:  true,
+		Progress:      os.Stderr,
+	})
+	return err
+}
--- internal/config/parser.go
+++ internal/config/parser.go
@@ -10,6 +10,10 @@ import (
 )
 
 func ParseModuleFromFile(filePath string) (*Module, error) {
+	return parseModuleFromFile(filePath)
+}
+
+func parseModuleFromFile(filePath string) (*Module, error) {
 	moduleFile := ModuleFile{}
 	module := Module{}
 
@@ -44,7 +48,21 @@ func ParseModuleFromFile(filePath string) (*Module, error) {
 		if ok {
 			return nil, fmt.Errorf("parsing %v: trying to import module %v, but it already exists", filePath, import_name)
 		}
-		submodule, err := ParseModuleFromFile(path.Join(path.Dir(filePath), import_obj.From, "Ebro.yaml"))
+		module_file_path := path.Join(path.Dir(filePath), import_obj.From, "Ebro.yaml")
+		git_refernce, err := parseGitReference(import_obj.From)
+		if err != nil {
+			return nil, fmt.Errorf("parsing possible git reference %v in file %v: %w", import_obj.From, filePath, err)
+		}
+
+		if git_refernce != nil {
+			err := gitCloneModule(git_refernce)
+			if err != nil {
+				return nil, fmt.Errorf("cloning %v: %w", import_name, err)
+			}
+			module_file_path = path.Join(git_refernce.clonePath, git_refernce.subPath, "Ebro.yaml")
+		}
+
+		submodule, err := parseModuleFromFile(module_file_path)
 		if err != nil {
 			return nil, fmt.Errorf("parsing %v: %w", filePath, err)
 		}
