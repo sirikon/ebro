b3c01edc291ea4acabe5f22981b133cd8ba1362c
Carlos Fdez. Llamas <hello@sirikon.me>
2024-12-21 22:50:09 +0100

    rename build process from docs to website

--- meta/website/_/src/ebro_website/freeze.py
+++ meta/website/_/src/ebro_website/freeze.py
@@ -2,9 +2,9 @@ from os import getcwd
 from os.path import join
 
 from flask_frozen import Freezer
-from ebro_docs.app import app
+from ebro_website.app import app
 
-app.config["FREEZER_DESTINATION"] = join(getcwd(), "out", "docs")
+app.config["FREEZER_DESTINATION"] = join(getcwd(), "out", "website")
 freezer = Freezer(app)
 
 if __name__ == "__main__":
--- meta/website/build.sh
+++ meta/website/build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 ./meta/python/ensure-venv.sh
 PYTHONPATH="$(realpath "$(dirname "${BASH_SOURCE[0]}")")/_/src"
 export PYTHONPATH
-exec ./meta/python/_/.venv/bin/python -m ebro_docs.freeze
+exec ./meta/python/_/.venv/bin/python -m ebro_website.freeze
