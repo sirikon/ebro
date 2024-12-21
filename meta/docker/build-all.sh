b932cb79014de307fce0b4dbb4691c6b78ef7f9c
Carlos Fdez. Llamas <hello@sirikon.me>
2024-12-21 20:32:57 +0100

    Include documentation building in Dockerfile

--- meta/docker/build-all.sh
+++ meta/docker/build-all.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 GO_VERSION="$(grep go <.tool-versions | sed -E 's/^go (.*)$/\1/g')"
+PYTHON_VERSION="$(grep python <.tool-versions | sed -E 's/^python (.*)$/\1/g')"
+POETRY_VERSION="$(grep poetry <.tool-versions | sed -E 's/^poetry (.*)$/\1/g')"
 TAG="ebro-build:$(base64 </dev/urandom | tr -d '[A-Z]+/' | head -c 8 || true)"
 
 rm -rf out
@@ -9,6 +11,8 @@ docker build \
     -t "$TAG" \
     --file meta/docker/_/Dockerfile \
     --build-arg "GO_VERSION=${GO_VERSION}" \
+    --build-arg "PYTHON_VERSION=${PYTHON_VERSION}" \
+    --build-arg "POETRY_VERSION=${POETRY_VERSION}" \
     .
 container_id="$(docker create "$TAG")"
 docker cp "$container_id:/out" ./out
