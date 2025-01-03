e516baf8f654dd758cbbaf6a6656613fa793e9f3
Carlos Fdez. Llamas <hello@sirikon.me>
2025-01-03 16:31:20 +0100

    Changes to update-dependencies.sh script

--- meta/update-dependencies.sh
+++ meta/update-dependencies.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd src
-go get -u ./...
-go mod tidy
+echo "Updating Go dependencies"
+(
+  cd src
+  go get -u ./...
+  go mod tidy
+)
+
+echo "Updating Python dependencies"
+./meta/python/poetry.sh update
