1ca0027d6af585d74e5c375b0ed649bc51bd8447
Carlos Fdez. Llamas <hello@sirikon.me>
2025-02-04 20:52:37 +0100

    more scenarios

--- tests/src/plan_test.py
+++ tests/src/plan_test.py
@@ -3,22 +3,6 @@ from utils.common import EbroTestCase
 
 class TestPlan(EbroTestCase):
 
-    def test_plan_for_cyclic_task_fails_with_correct_help(self):
-        exit_code, stdout = self.ebro("-plan", "--file", "Ebro.cyclic.yaml")
-        self.assertEqual(exit_code, 1)
-        self.assertStdout(
-            stdout,
-            """
-            ███ ERROR: planning could not complete. there could be a cyclic dependency. here is the list of tasks remaining to be planned and their requirements:
-            :chicken:
-            - :egg
-            :default:
-            - :chicken
-            :egg:
-            - :chicken
-            """,
-        )
-
     def test_plan_for_wrong_references_1(self):
         exit_code, stdout = self.ebro("-plan", "--file", "Ebro.wrong_references_1.yaml")
         self.assertEqual(exit_code, 1)
@@ -28,7 +12,7 @@ class TestPlan(EbroTestCase):
             ███ ERROR: normalizing 'requires' for task ':default': referenced task ':nonexistent' does not exist
             """,
         )
-    
+
     def test_plan_for_wrong_references_2(self):
         exit_code, stdout = self.ebro("-plan", "--file", "Ebro.wrong_references_2.yaml")
         self.assertEqual(exit_code, 1)
@@ -38,7 +22,7 @@ class TestPlan(EbroTestCase):
             ███ ERROR: normalizing 'required_by' for task ':default': referenced task ':nonexistent' does not exist
             """,
         )
-    
+
     def test_plan_for_wrong_references_3(self):
         exit_code, stdout = self.ebro("-plan", "--file", "Ebro.wrong_references_3.yaml")
         self.assertEqual(exit_code, 1)
