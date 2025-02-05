5e349f8bad1de14c9f665d975d83aa2ed47364ec
Carlos Fdez. Llamas <hello@sirikon.me>
2025-02-05 13:11:09 +0100

    remove remaining remote git import stuff

--- tests/src/utils/common.py
+++ tests/src/utils/common.py
@@ -1,66 +1,8 @@
-import json
-from time import sleep
 import unittest
 from os import getcwd, environ
 from os.path import join, normpath
 from subprocess import run, PIPE, STDOUT
 from textwrap import dedent
-import http.client
-
-
-def fake_git_server(func):
-    def wrapper(*args, **kwargs):
-        result = _docker(
-            "run",
-            "-d",
-            "-p",
-            "80",
-            "-v",
-            join(getcwd(), "..", "playground") + ":/content:ro",
-            "-v",
-            join(getcwd(), "./fake_git_server/entrypoint.sh") + ":/entrypoint.sh:ro",
-            "ynohat/git-http-backend",
-            "/entrypoint.sh",
-        )
-        container_id = result.stdout.decode("utf-8").strip()
-        container_info = _docker_inspect(container_id)
-        local_port = container_info[0]["NetworkSettings"]["Ports"]["80/tcp"][0][
-            "HostPort"
-        ]
-        repository_url = f"http://127.0.0.1:{local_port}/git/fake.git"
-        retries = 300
-        while retries > 0:
-            retries = retries - 1
-            try:
-                sleep(0.01)
-                conn = http.client.HTTPConnection(f"127.0.0.1:{local_port}")
-                conn.request("GET", "/")
-                conn.getresponse()
-                conn.close()
-                break
-            except Exception:
-                pass
-        try:
-            func(*args, repository_url, **kwargs)
-        finally:
-            _docker("rm", "-f", container_id)
-
-    return wrapper
-
-
-def _docker_inspect(id):
-    result = _docker("inspect", id)
-    return json.loads(result.stdout)
-
-
-def _docker(*args):
-    return run(
-        ["docker", *args],
-        check=True,
-        stdin=None,
-        stdout=PIPE,
-        stderr=PIPE,
-    )
 
 
 class EbroTestCase(unittest.TestCase):
